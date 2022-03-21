15253f8e4521d5579f582cabb57944398e651882
szafik <szafrann.michal@gmail.com>
2022-03-21 18:49:31 +0000

    11

---
+++ moje_programy/character_wiki.py
@@ -0,0 +1,14 @@
+
+import wikipedia as wiki
+wiki.set_lang('pl')
+
+def character(name):
+
+    content = wiki.summary(name, sentences=6)
+    return content
+
+
+name = 'Bruce Lee'
+print(character(name))
+
+    
